de74eeba1cbd932b98a9656c897e5bef0c475c43
sseerr1 <46776271+sseerr1@users.noreply.github.com>
2025-01-07 13:06:32 +0300

    Add files via upload

---
+++ module_13_5.py
@@ -0,0 +1,75 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = "7772840862:AAEDn7Ofh69V9-YP0e767wpKw9Ij94Um0mY"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот, помогающий твоему здоровью.', reply_markup=kb)
+
+
+# Keyboard
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Информация')
+button2 = KeyboardButton(text='Рассчитать')
+kb.row(button2)
+kb.insert(button1)
+
+
+@dp.message_handler(text="Рассчитать")
+async def set_age(message: types.Message, state: FSMContext):
+    await UserState.age.set()
+    await message.reply("Введите свой возраст:")
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await UserState.next()
+    await message.reply("Введите свой рост:")
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)
+    await UserState.next()
+    await message.reply("Введите свой вес:")
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+
+    # Упрощенная формула Миффлина - Сан Жеора для мужчин
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+
+    await message.reply(f"Ваша норма калорий: {calories} ккал")
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
